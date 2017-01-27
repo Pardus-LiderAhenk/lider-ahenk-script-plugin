0a52080413a5e150852f17f4f6745b0721abdc95
tncyclk <tuncay.colak@tubitak.gov.tr>
2017-01-27 15:22:25 +0300

    fix 1.0.0-beta version

--- lider-console-script/src/tr/org/liderahenk/script/constants/ScriptConstants.java
+++ lider-console-script/src/tr/org/liderahenk/script/constants/ScriptConstants.java
@@ -4,7 +4,7 @@ public class ScriptConstants {
 
 	public static final String PLUGIN_NAME = "script";
 
-	public static final String PLUGIN_VERSION = "1.0.0";
+	public static final String PLUGIN_VERSION = "1.0.0-beta";
 
 	public static final class EDITORS {
 		public static final String SCRIPT_DEFINITON_EDITOR = "tr.org.liderahenk.script.editors.ScriptDefinitionEditor";
